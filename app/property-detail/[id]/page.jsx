abdc2241ffa55585954730bdcd8045af49048f83
eatiakoode <eati@akoode.in>
2025-06-21 16:42:57 +0530

    thumbnail iamge

--- app/property-detail/[id]/page.jsx
+++ app/property-detail/[id]/page.jsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import "photoswipe/dist/photoswipe.css";
 import { getPropertyBySlug } from "@/api/frontend/property";
 import PropertyMain from "@/components/listing-details-v1";
