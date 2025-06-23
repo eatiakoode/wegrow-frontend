102a2618676ee6a1a80b401959b20f116981a242
eatiakoode <eati@akoode.in>
2025-06-23 13:24:50 +0530

    cahche issue

---
+++ api/cityglimpse.ts
@@ -0,0 +1,166 @@
+export const addCityGlimpseAPI = async (fromdata) => {
+    // const token = localStorage.getItem("token"); // 🔹 Retrieve token
+
+    // const token =process.env.NEXT_PUBLIC_TOKEN;
+    const userData = JSON.parse(localStorage.getItem("user"));
+console.log(userData.name);
+// const token = localStorage.getItem("token"); // 🔹 Retrieve token
+// // console.log("token")
+//     const token =process.env.NEXT_PUBLIC_TOKEN;
+const token =userData.token
+  
+    if (!token) {
+      throw new Error("User not authenticated!");
+    }
+  
+    const response = await fetch(process.env.NEXT_PUBLIC_ADMIN_API_URL+"api/cityglimpse", {
+      method: "POST",
+      headers: {
+        // "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: fromdata,
+    });
+  
+    if (!response.status) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "Failed to add property plan");
+    }
+  
+    return response.json();
+  };
+  
+
+  export async function getCityGlimpseTableData() {
+    // Fake delay
+    await new Promise((resolve) => setTimeout(resolve, 1400));
+  
+    try {
+      const response = await fetch(process.env.NEXT_PUBLIC_ADMIN_API_URL+"api/cityglimpse"); // Replace with actual API endpoint
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      return []; // Return an empty array in case of an error
+    }
+  }
+
+
+  export const deleteCityGlimpseAPI = async (id: string) => {
+    // const token = localStorage.getItem("token"); // 🔹 Retrieve token
+
+
+    // const token =process.env.NEXT_PUBLIC_TOKEN;
+    const userData = JSON.parse(localStorage.getItem("user"));
+console.log(userData.name);
+// const token = localStorage.getItem("token"); // 🔹 Retrieve token
+// // console.log("token")
+//     const token =process.env.NEXT_PUBLIC_TOKEN;
+const token =userData.token
+    if (!token) {
+      throw new Error("User not authenticated!");
+    }
+  
+    const response = await fetch(process.env.NEXT_PUBLIC_ADMIN_API_URL+`api/cityglimpse/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ id }),
+    });
+  
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "Failed to delete property plan");
+    }
+  
+    return response.json();
+  };
+
+
+  
+  
+
+  export const getCityGlimpseById = async (id: string) => {
+    // const token = localStorage.getItem("token"); // 🔹 Retrieve token
+
+
+    // const token =process.env.NEXT_PUBLIC_TOKEN;
+    const userData = JSON.parse(localStorage.getItem("user"));
+console.log(userData.name);
+// const token = localStorage.getItem("token"); // 🔹 Retrieve token
+// // console.log("token")
+//     const token =process.env.NEXT_PUBLIC_TOKEN;
+const token =userData.token
+    if (!token) {
+      throw new Error("User not authenticated!");
+    }
+  
+    const response = await fetch(process.env.NEXT_PUBLIC_ADMIN_API_URL+`api/cityglimpse/byid/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      // body: JSON.stringify({ id }),
+    });
+  
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "Failed to get cityglimpse");
+    }
+  
+    return response.json();
+  };
+
+
+  export const updateCityGlimpseAPI = async (id,cityglimpse) => {
+    // const token = localStorage.getItem("token"); // 🔹 Retrieve token
+
+    // const token =process.env.NEXT_PUBLIC_TOKEN;
+    const userData = JSON.parse(localStorage.getItem("user"));
+console.log(userData.name);
+// const token = localStorage.getItem("token"); // 🔹 Retrieve token
+// // console.log("token")
+//     const token =process.env.NEXT_PUBLIC_TOKEN;
+const token =userData.token
+    if (!token) {
+      throw new Error("User not authenticated!");
+    }
+  
+    const response = await fetch(process.env.NEXT_PUBLIC_ADMIN_API_URL+`api/cityglimpse/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(cityglimpse),
+    });
+  
+    if (!response.status) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "Failed to add cityglimpse");
+    }
+  
+    return response.json();
+  };
+
+  export const  getCityGlimpseByCategoryTableData = async (id: string) => {
+    // Fake delay
+    await new Promise((resolve) => setTimeout(resolve, 1400));
+  
+    try {
+      const response = await fetch(process.env.NEXT_PUBLIC_ADMIN_API_URL+`api/cityglimpse/bycategory/${id}`); // Replace with actual API endpoint
+      
+      if (!response.ok) {
+        throw new Error("Failed to fetch property plan");
+      }
+      return await response.json();
+    } catch (error) {
+      console.error("Error fetching property plan:", error);
+      return []; // Return an empty array in case of an error
+    }
+  };
