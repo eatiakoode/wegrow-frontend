5c26b6fb7d88b481491cd84dcd2ba49bafbc4c17
tej-akoode123 <tejveer@akoode.in>
2025-06-30 18:42:03 +0530

    social media link updated

--- components/common/footer/Social.jsx
+++ components/common/footer/Social.jsx
@@ -1,10 +1,12 @@
 const Social = () => {
   const socialContent = [
-    { id: 1, liveLink: "#", icon: "fa-facebook" },
-    { id: 2, liveLink: "#", icon: "fa-x-twitter" },
-    { id: 3, liveLink: "#", icon: "fa-instagram" },
-    { id: 4, liveLink: "#", icon: "fa-pinterest" },
+    { id: 1, liveLink: "https://www.facebook.com/WeGrowInfraventurespvtltd/", icon: "fa-facebook" },
+    { id: 2, liveLink: "https://x.com/wegrowinfra/", icon: "fa-x-twitter" },
+    { id: 3, liveLink: "https://www.instagram.com/wegrowinfraventures/", icon: "fa-instagram" },
+    { id: 4, liveLink: "https://www.linkedin.com/company/wegrow-infraventures-pvt-ltd/", icon: "fa-linkedin-in" },
   ];
+
+  
   return (
     <>
       {socialContent.map((item) => (
