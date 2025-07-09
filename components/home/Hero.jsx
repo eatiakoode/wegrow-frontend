3b6353b766bfa98773426f10d73c6129802fea23
eatiakoode <eati@akoode.in>
2025-07-09 15:45:14 +0530

    category and update

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -13,7 +13,7 @@ const Hero = () => {
           loop
           playsInline
         >
-          <source src="/assets/images/myvideo-compressed.mp4" type="video/mp4" />
+          <source src={`${process.env.NEXT_PUBLIC_API_URL}public/assets/images/background/myvideo-compressed.mp4`} type="video/mp4" />
           {/* Your browser does not support the video tag. */}
         </video>
       </div>
