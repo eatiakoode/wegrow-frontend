51d43e8569d0d3c1d5f52744503ed21553e18804
tej-akoode123 <tejveer@akoode.in>
2025-07-21 23:13:10 +0530

    about us page updated

--- components/about-us/Team.jsx
+++ components/about-us/Team.jsx
@@ -9,7 +9,7 @@ const Team = () => {
   const settings = {
     dots: false,
     arrows: true,
-    slidesToShow: 4,
+    slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: false,
     speed: 1200,
